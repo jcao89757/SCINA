78c4568503ac55870af11aa14f4db1fd7a56e451
Ze Zhang <Ze.Zhang@UTSouthwestern.edu>
2018-09-20 17:01:17 -0500

    Add files via upload

--- R/EM_functions.R
+++ R/EM_functions.R
@@ -1,4 +1,4 @@
-EM_check=function(exp,signatures,max_iter,convergence_n,convergence_rate,sensitivity_cutoff,rm_overlap)
+check.inputs=function(exp, signatures, max_iter, convergence_n, convergence_rate, sensitivity_cutoff, rm_overlap, log_file)
 {
   # initialize parameters
   quality=1
@@ -7,26 +7,19 @@ EM_check=function(exp,signatures,max_iter,convergence_n,convergence_rate,sensiti
   def_conv_rate=0.99
   def_dummycut=0.33
   allgenes=row.names(exp)
-
-  # clean exp
-  if (any(is.na(exp)))
-  {
-    cat('NA exists in expression matrix.',file='SCINA.log',append=T,sep='\n')
+  # Check exp
+  if (any(is.na(exp))){
+    cat('NA exists in expression matrix.',file=log_file,append=T,sep='\n')
     quality=0
   }
-
-  # clean signatures
-  if (any(is.na(signatures)))
-  {
-    cat('Null cell type signature genes.',file='SCINA.log',append=T,sep='\n');quality=0
-  }else
-  {
-    #na.omit(NAs) in each signatures,remove unvalid genes (not in exp)
+  # Check signatures
+  if (any(is.na(signatures))){
+    cat('Null cell type signature genes.',file=log_file,append=T,sep='\n');quality=0
+  }else{
+    #na.omit(NAs) in each signatures, remove unvalid genes (not in exp)
     signatures=sapply(signatures,function(x) unique(x[(!is.na(x)) & (x %in% allgenes)]),simplify=F)
-
     #remove duplicate genes
-    if(rm_overlap==1)
-    {
+    if(rm_overlap==1){
       tmp=table(unlist(signatures))
       signatures=sapply(signatures,function(x) x[x %in% names(tmp[tmp==1])],simplify=F)
     }
@@ -34,38 +27,30 @@ EM_check=function(exp,signatures,max_iter,convergence_n,convergence_rate,sensiti
     signatures=sapply(signatures,function(x) x[apply(exp[x,,drop=F],1,sd)>0],simplify=F)
   }
   # clean other parameters
-  if (is.na(convergence_n))
-  {
-    cat('Using convergence_n=default',file='SCINA.log',append=T,sep='\n')
+  if (is.na(convergence_n)){
+    cat('Using convergence_n=default',file=log_file,append=T,sep='\n')
     convergence_n=def_conv_n
   }
-  if (is.na(max_iter))
-  {
-    cat('Using max_iter=default',file='SCINA.log',append=T,sep='\n')
+  if (is.na(max_iter)){
+    cat('Using max_iter=default',file=log_file,append=T,sep='\n')
     max_iter=def_max_iter
-  }else
-  {
-    if (max_iter<convergence_n)
-    {
-      cat('Using max_iter=default due to smaller than convergence_n.',file='SCINA.log',append=T,sep='\n')
-      max_iter=def_max_iter # this theorectically, still doesn't ensure max_iter>convergence_n (Tao)
+  }else{
+    if (max_iter<convergence_n){
+      cat('Using max_iter=default due to smaller than convergence_n.',file=log_file,append=T,sep='\n')
+      max_iter=convergence_n # this theorectically, still doesn't ensure max_iter>convergence_n (Tao)
     }
   }
-
-  if (is.na(convergence_rate))
-  {
-    cat('Using convergence_rate=default.',file='SCINA.log',append=T,sep='\n')
+  if (is.na(convergence_rate)){
+    cat('Using convergence_rate=default.',file=log_file,append=T,sep='\n')
     convergence_rate=def_conv_rate
   }
-
-  if (is.na(sensitivity_cutoff))
-  {
-    cat('Using sensitivity_cutoff=default.',file='SCINA.log',append=T,sep='\n')
+  if (is.na(sensitivity_cutoff)){
+    cat('Using sensitivity_cutoff=default.',file=log_file,append=T,sep='\n')
     sensitivity_cutoff=def_dummycut
   }
   # return cleaned data
   return(list(qual=quality,sig=signatures,
-    para=c(max_iter,convergence_n,convergence_rate,sensitivity_cutoff)))
+              para=c(max_iter,convergence_n,convergence_rate,sensitivity_cutoff)))
 }
 
 density_ratio=function(e,mu1,mu2,inverse_sigma1,inverse_sigma2){
@@ -76,14 +61,6 @@ density_ratio=function(e,mu1,mu2,inverse_sigma1,inverse_sigma2){
   tmp[tmp<1e-200]=1e-200
   return(tmp)
 }
-
-#' Provide proper gene signatures depending on labeled cell population sequencing results.
-#'
-#'@param batched_mat #' A list of lists, contains all labeled cells, each cell type has an expression matrix with rows represent genes and columns represent cells. Each matrix is assigned in a daughter list with its cell type as the name of the list.
-#'@param cells #' A list of cell types need to generate gene signatures from.
-#'@param n #' An integer larger than 0, indicating the number of cells in each cell type for the purpose of signature generation.
-#'@param cutoff #' A float larger than 0 to control signature gene amounts for each of the cell types. A cutoff close to 0 will results in more genes in each signature. Default 1.5.
-#'@return signatures #' See more details in \code{\link{SCINA}}
 
 findgenesigs=function(batched_mat,cells,n,cutoff=1.5){
   mean_cell=c()
@@ -101,16 +78,7 @@ findgenesigs=function(batched_mat,cells,n,cutoff=1.5){
   return(signatures)
 }
 
-#' A function to plot SCINA results in a heatmap.
-#'
-#' @param exp See more details in \code{\link{SCINA}}
-#' @param results An output object returned from SCINA.
-#' @param signatures See more details in \code{\link{SCINA}}
-#' @return Plot A heatmap showing signature genes' expression level and SCINA predicted cell types.
-#' @examples
-#' plot.SCINA(exp,results,signatures)
-#'
-plot.SCINA=function(exp,results,signatures){
+plotheat.SCINA=function(exp,results,signatures){
   col_row=topo.colors(length(signatures))
   col_col=cm.colors(length(unique(results$cell_labels)))
   exp2plot=exp[unlist(signatures),order(factor(results$cell_labels,levels=c(names(signatures),'unknown')),decreasing=F)]
@@ -125,14 +93,10 @@ plot.SCINA=function(exp,results,signatures){
   return(invisible(plot))
 }
 
-#' A function to convert signatures uploaded via .csv files to lists used by SCINA.
-#'
-#' @param file_path The path where the .csv file stores. The first row of the file should be cell type names. Each column is occupied by the signature genes/protein markers for the cell type in the first row. Please find more details in \code{\link{SCINA}}.
-#' @return A list of signature gene lists as an input for SCINA.
-#' @examples
-#' signatures=preprocess.signatures('./example/example_signatures.csv')
 preprocess.signatures=function(file_path){
   csv_signatures=read.csv(file_path,stringsAsFactors = F,header = T,fill = F,quote ='')
   signatures=as.list(csv_signatures)
+  signatures=sapply(signatures,function(x) x[(!is.na(x)) & x!=""],simplify = F)
   return(signatures)
 }
+
--- R/EM_model.R
+++ R/EM_model.R
@@ -1,65 +1,11 @@
-#' @title A semi-supervised category identification and assignment tool.
-#'
-#' @description An automatic cell type detection and assignment algorithm for single cell RNA-Seq (scRNA-seq) and Cytof/FACS data. SCINA is capable of assigning cell type identities to a pool of cells profiled by scRNA-Seq or Cytof/FACS data with prior knowledge of identifiers, such as genes and protein symbols,
-#' that are highly or lowly expressed in each category. See Ze Z, Danni L, et al (2018) <doi:> for more details.
-#'
-#' @param exp A normalized matrix representing the target dataset. Columns correpond to objects (cell barcodes for example), rows correspond to attributes or variables (gene symbols for example).
-#' @details 
-#' For efficiency of data transfer and computation, the user is encouraged to upload the subset of the gene expression matrix that contains only the genes that appeared in the signature list.
-#' @param signatures A list contains multiple signature identifier lists. Each signature identifier list (genes for example) represents prior knowledge for one category (cell type for example), containing genes or protein symbols with high degree of detection.
-#' @details
-#' For any symbols in signature lists, if the category is identified with symbol X's low detection level, please specify the symbol as 'low_X'. The name for the list is the category.
-#' Details for 'low_X' (take scRNA-Seqs as an example):
-#' (a) There are 4 cell types, the first one highly express one gene A, and the other three lowly express the same gene. Then it is better to specify A as the high marker for cell type 1, but it is not a good idea to specify A as the low expression marker for cell type 2,3,4.\cr
-#' \if{html}{\figure{sub1.png}{options: width=0.5in}}
-#' \if{latex}{\figure{sub1.png}{options: width=0.5in}}
-#' (b) There are 4 cell types, the first one lowly express one gene A, and the other three highly express the same gene. Then is it better to specify A as the low marker for cell type 1, but it is not a good idea to specify A as the high expression marker for cell type 2,3,4.\cr
-#' \if{html}{\figure{sub2.png}{options: width=0.5in}}
-#' \if{latex}{\figure{sub2.png}{options: width=0.5in}}
-#' (c) There are 4 cell types, the first one lowly express one gene A, the second and third one moderately express gene A, and the last one highly express gene A. Then is it better to specify A as the low marker for cell type 1, and as the high expression marker for cell type 4.\cr
-#' \if{html}{\figure{sub3.png}{options: width=0.5in}}
-#' \if{latex}{\figure{sub3.png}{options: width=0.5in}}
-#' (d) The same specification can be applied to protein markers in CyTOF anlysis.\cr
-#' \if{html}{\figure{sub4.png}{options: width=0.5in}}
-#' \if{latex}{\figure{sub4.png}{options: width=0.5in}}
-#' For immune cell types, an example signature lists can be found in \code{\link[DisHet]{eTME_signatures}.
-#' @param max_iter An integer > 0. Default is 100. Max iterations allowed for the EM algorithm.
-#' @param covergence_n An integer > 0. Default is 10. Stop the EM algorithm if during the last n rounds of iterations, cell type assignment keeps steady above the convergence_rate.
-#' @param convergence_rate A float between 0 and 1. Default is 0.99. Percentage of cells for which the type assignment remains stable for the last n rounds. 
-#' @param sensitivity_cutoff A float between 0 and 1. Default is 1. The cutoff to remove signatures whose cells types are deemed as non-existent at all in the data by the SCINA algorithm. 
-#' @details
-#' Small sensitivity_cutoff leads to more signatures to be removed, and 1 denotes that no signature is removed.
-#' @param rm_overlap A binary value, default 1 (TRUE), denotes that shared symbols between signature lists will be removed. If 0 (FALSE) then allows different cell types to share the same identifiers.
-#' @param allow_unknown A binary value, default 1 (TRUE). If 0 (FALSE) then no cell will be assigned to the 'unknown' category.
-#'
-#' @return cell_labels A vector contains cell type mapping results for each cell.
-#' @return probabilities A probability matrix indicating the predicted probability for each cell belongs to each cell type respectively.
-#' @import MASS
-#' @importFrom gplots heatmap.2
-#' @importFrom Rdpack reprompt
-#' @export SCINA
-#' @export plot.SCINA
-#' @export preprocess.signatures
-#' @examples
-
-#' load(system.file("example/example_expmat.RData",package="SCINA"))
-#' load(system.file("example/example_signatures.RData",package="SCINA"))
-#' exp=exp_test$exp_data
-#' results=SCINA(exp,signatures,max_iter=120,convergence_n=12,convergence_rate=0.999,sensitivity_cutoff=0.9)
-#' table(exp_test$true_label,results$cell_labels)
-
-#' @references
-#' \insertRef{@Rpack:bibtex}{Rdpack}
-#'
 SCINA=function(exp,signatures,max_iter=100,convergence_n=10,convergence_rate=0.99,
-               sensitivity_cutoff=1,rm_overlap=1,allow_unknown=1)
-{
-  cat('Start running SCINA.',file='SCINA.log',append=F)
-  #check for low-expression signatures
+               sensitivity_cutoff=1,rm_overlap=1,allow_unknown=1,log_file='SCINA.log')
+  {
+  cat('Start running SCINA.',file=log_file,append=F)
   all_sig=unique(unlist(signatures))
   invert_sigs=grep('^low_',all_sig,value=T)
   if(!identical(invert_sigs, character(0))){
-    cat('Converting expression matrix for low_genes.',file='SCINA.log',append=T)
+    cat('Converting expression matrix for low_genes.',file=log_file,append=T)
     invert_sigs_2add=unlist(lapply(invert_sigs,function(x) strsplit(x,'_')[[1]][2]))
     invert_sigs=invert_sigs[invert_sigs_2add%in%row.names(exp)]# To avoid users adding 'low_' to non-existing genes.
     invert_sigs_2add=invert_sigs_2add[invert_sigs_2add%in%row.names(exp)]
@@ -68,9 +14,8 @@ SCINA=function(exp,signatures,max_iter=100,convergence_n=10,convergence_rate=0.9
     exp=rbind(exp,sub_exp)
     rm(sub_exp,all_sig,invert_sigs,invert_sigs_2add)
   }
-  #invalid parameters check
-  quality=EM_check(exp,signatures,max_iter,convergence_n,convergence_rate,sensitivity_cutoff,rm_overlap)
-  if(quality$qual==0){cat('EXITING due to invalid parameters.',file='SCINA.log',append=T,sep='\n');stop('SCINA stopped.')}
+  quality=check.inputs(exp,signatures,max_iter,convergence_n,convergence_rate,sensitivity_cutoff,rm_overlap,log_file)
+  if(quality$qual==0){cat('EXITING due to invalid parameters.',file=log_file,append=T,sep='\n');stop('SCINA stopped.')}
   signatures=quality$sig
   max_iter=quality$para[1]
   convergence_n=quality$para[2]
@@ -83,7 +28,7 @@ SCINA=function(exp,signatures,max_iter=100,convergence_n=10,convergence_rate=0.9
   unsatisfied=1
   if(allow_unknown==1){
     tao=rep(1/(length(signatures)+1),length(signatures))
-    }else{tao=rep(1/(length(signatures)),length(signatures))}
+  }else{tao=rep(1/(length(signatures)),length(signatures))}
   theta=list()
   for(i in 1:length(signatures)){
     theta[[i]]=list()
@@ -134,17 +79,16 @@ SCINA=function(exp,signatures,max_iter=100,convergence_n=10,convergence_rate=0.9
         tmp2=t((exp[signatures[[i]],,drop=F]-theta[[i]]$mean[,2])^2)
         diag(theta[[i]]$sigma1)=diag(theta[[i]]$sigma2)=
           colSums(tmp1*prob_mat[i,]+tmp2*(1-prob_mat[i,]))/dim(prob_mat)[2]
-        if (sum(prob_mat[i,])==0) {theta[[i]]$sigma1=theta[[i]]$sigma2}
         diag(theta[[i]]$sigma1)[diag(theta[[i]]$sigma1)<sigma_min]=sigma_min
         diag(theta[[i]]$sigma2)[diag(theta[[i]]$sigma2)<sigma_min]=sigma_min
       }
 
       labels[,labels_i]=apply(rbind(1-colSums(prob_mat),prob_mat),2,which.max)-1
       if(mean(apply(labels,1,function(x) length(unique(x))==1))>=convergence_rate)
-      {cat('Job finished successfully.',file='SCINA.log',append=T,sep='\n');break}
+      {cat('Job finished successfully.',file=log_file,append=T,sep='\n');break}
       labels_i=labels_i+1
       if(labels_i==convergence_n+1){labels_i=1}
-      if(iter==max_iter){cat('Maximum iterations, breaking out.',file='SCINA.log',append=T,sep='\n')}
+      if(iter==max_iter){cat('Maximum iterations, breaking out.',file=log_file,append=T,sep='\n')}
     }
     colnames(prob_mat)=colnames(exp)
     row.names(prob_mat)=names(signatures)
@@ -157,7 +101,7 @@ SCINA=function(exp,signatures,max_iter=100,convergence_n=10,convergence_rate=0.9
     }else
     {
       rev=which(dummytest>sensitivity_cutoff);
-      cat(paste('Remove dummy signatures:',rev,sep=' '),file='SCINA.log',append=T,sep='\n');
+      cat(paste('Remove dummy signatures:',rev,sep=' '),file=log_file,append=T,sep='\n');
       # note the changes below
       signatures=signatures[-rev]
       tmp=1-sum(tao)
